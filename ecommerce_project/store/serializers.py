1f7469b0fa9aa23232c481b04dbd7749de09a726
Steve Chege <chegekstephen@gmail.com>
2025-04-06 08:21:53 +0300

    Implemented Order serializer

--- ecommerce_project/store/serializers.py
+++ ecommerce_project/store/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Category, Product
+from .models import Category, Product, Order
+from django.contrib.auth.models import User
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -14,3 +15,19 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    product = serializers.StringRelatedField()
+    user = serializers.StringRelatedField()
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
